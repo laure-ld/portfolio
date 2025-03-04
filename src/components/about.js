eef9adfae31f984cb3ef9bbac441aab5d3dbca49
Laure <laure_ld@outlook.fr>
2025-03-04 15:07:20 +0100

    Creaction partie about

---
+++ src/components/about.js
@@ -0,0 +1,43 @@
+import { useRef, useEffect } from 'react';
+import '../styles/main.scss';
+
+
+function About() {
+
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const video = videoRef.current;
+        video.play();
+
+        const handleVideoEnd = () => {
+            video.pause();
+            video.currentTime = video.duration - 0.1;
+        };
+
+        video.addEventListener('ended', handleVideoEnd);
+
+        return () => {
+            video.removeEventListener('ended', handleVideoEnd);
+        };
+    }, []);
+
+    return (
+    <section class="about-me">
+        <div className="about-media">
+                <video ref={videoRef} src="me.mp4" muted />
+        </div>
+        <div class="about-content">
+            <h2>À propos de moi</h2>
+            <ul>
+                <li><strong>Formation :</strong> Bac+X en [Domaine]</li>
+                <li><strong>Langages :</strong> HTML, CSS, JS, React...</li>
+                <li><strong>Objectif :</strong> Devenir [ton objectif, ex: Dev Fullstack ou Dev Blockchain]</li>
+            </ul>
+            <a href="/chemin/vers/cv.pdf" download class="download-btn">Télécharger mon CV</a>
+        </div>
+    </section>
+    )
+}
+
+export  default About;
---
+++ src/components/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import About from "./about";
+
+function Main() {
+    return( 
+        <section className="contenair-main">
+            <About />
+        </section>
+    )
+}
+
+export default Main;
