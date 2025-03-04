6541da95564ba7baeca1a8663a95f83195c1da8a
Laure <laure_ld@outlook.fr>
2025-03-04 12:10:31 +0100

    1er commit

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import '../styles/header.scss';
+
+function Header () {
+    return (
+        <div className="container">
+            <p>Portfolio</p>
+            <ul>
+                <li>Mes projets</li>
+                <li>Mes compétences</li>
+                <li>Informations</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header; 
