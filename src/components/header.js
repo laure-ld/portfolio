9ed5c865214035a4f2ee6dd569847bbcc4df00dd
Laure <laure_ld@outlook.fr>
2025-03-05 23:57:08 +0100

    Correction du header et crea menu hamburger

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,30 @@
 import  { Link } from 'react-router-dom';
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/header.scss';
 
 function Header () {
+    const [isOpen, setIsOpen] = useState(false);
+    function handleOnClick() {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <section className="container">
             <p>Portfolio</p>
-            <div className='menu'>
-                <Link to="/">Mes projets</Link>
-                <Link to="/">Mes compétences</Link>
-                <Link to="/">Mes informations</Link>
-            </div>
+            <button aria-expanded={isOpen} aria-controls="menu" onClick={handleOnClick} className="burger-btn">
+                    <svg  className='line' stroke="black" fill="none" strokeLinecap="round" height="28" width="28" xmlns="http://www.w3.org/2000/svg">
+                        <line x1="3" y1="12" x2="21" y2="12" strokeWidth="2"/>
+                        <line x1="3" y1="6" x2="21" y2="6" strokeWidth="2"/>
+                        <line x1="3" y1="18" x2="21" y2="18" strokeWidth="2"/>
+                    </svg>
+            </button>
+            <nav id="menu" className={`menu ${isOpen ? 'open' : ''}`} >
+                <ul>
+                    <li><Link to="/">Mes projets</Link></li>
+                    <li><Link to="/about">À propos</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </nav>
         </section>
     )
 }
