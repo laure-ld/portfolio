aef250ac45595cec6bfd3123125372cacf2f9ba1
Laure <laure_ld@outlook.fr>
2025-03-06 16:40:54 +0100

    Travail sur la partie about

--- src/components/about.js
+++ src/components/about.js
@@ -1,42 +1,20 @@
-import { useRef, useEffect } from 'react';
 import '../styles/main.scss';
 
-
 function About() {
-
-    const videoRef = useRef(null);
-
-    useEffect(() => {
-        const video = videoRef.current;
-        video.play();
-
-        const handleVideoEnd = () => {
-            video.pause();
-            video.currentTime = video.duration - 0.1;
-        };
-
-        video.addEventListener('ended', handleVideoEnd);
-
-        return () => {
-            video.removeEventListener('ended', handleVideoEnd);
-        };
-    }, []);
-
     return (
     <section class="about-me">
-        <div className="about-media">
-                <video ref={videoRef} src='itsme.mp4' muted />
-        </div>
         <div class="about-content">
-            <h2>À propos de moi</h2>
-            <ul>
-                <li><strong>Formation :</strong> Bac+X en [Domaine]</li>
-                <li><strong>Langages :</strong> HTML, CSS, JS, React...</li>
-                <li><strong>Objectif :</strong> Devenir [ton objectif, ex: Dev Fullstack ou Dev Blockchain]</li>
-            </ul>
-            <div className='button'>
-                <a href="/chemin/vers/cv.pdf" download class="download-btn">Télécharger mon CV</a>
-            </div>
+            <h1>Laure Loda</h1>
+            <h2>Développeur Web</h2>
+            <p>Passionnée par le développement web, j’ai d’abord suivi un parcours en psychologie et ergonomie des IA avant de me tourner pleinement vers cette voie.Aujourd’hui, je me forme avec un objectif clair : devenir Architecte Blockchain.</p>
+            <div className='buttons'> 
+                <div className='button-container'>
+                    <a href="/chemin/vers/cv.pdf" download class="download-btn">CV</a>
+                </div>
+                <div className='button-container'>
+                    <a href="/chemin/vers/cv.pdf" download class="download-btn">GitHub</a>
+                </div>
+            </div>   
         </div>
     </section>
     )
