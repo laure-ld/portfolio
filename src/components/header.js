8c7c94353ac6374028b919f2ef3a50b58a10d990
Laure <laure_ld@outlook.fr>
2025-03-06 17:33:45 +0100

    Travail sur  le menu de nav

--- src/components/header.js
+++ src/components/header.js
@@ -13,9 +13,9 @@ function Header () {
             <p>Portfolio</p>
             <button aria-expanded={isOpen} aria-controls="menu" onClick={handleOnClick} className="burger-btn">
                     <svg  className='line' stroke="black" fill="none" strokeLinecap="round" height="28" width="28" xmlns="http://www.w3.org/2000/svg">
-                        <line x1="3" y1="12" x2="21" y2="12" strokeWidth="2"/>
-                        <line x1="3" y1="6" x2="21" y2="6" strokeWidth="2"/>
-                        <line x1="3" y1="18" x2="21" y2="18" strokeWidth="2"/>
+                        <line x1="4" y1="12" x2="25" y2="12" strokeWidth="2"/>
+                        <line x1="4" y1="6" x2="25" y2="6" strokeWidth="2"/>
+                        <line x1="4" y1="18" x2="25" y2="18" strokeWidth="2"/>
                     </svg>
             </button>
             <nav id="menu" className={`menu ${isOpen ? 'open' : ''}`} >
