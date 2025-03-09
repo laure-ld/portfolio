e4656c2b54193694dab7be64bb73e90cf44aa508
Laure <laure_ld@outlook.fr>
2025-03-09 21:55:57 +0100

    mise en place d'une animation circulaire

--- src/components/about.js
+++ src/components/about.js
@@ -2,19 +2,27 @@ import '../styles/main.scss';
 
 function About() {
     return (
-    <section class="about-me">
-        <div class="about-content">
+    <section className="about-me">
+        <div className="about-content">
             <h1>Laure Loda</h1>
             <h2>Développeur Web</h2>
             <p>Passionnée par le développement web, j’ai d’abord suivi un parcours en psychologie et ergonomie des IA avant de me tourner pleinement vers cette voie.Aujourd’hui, je me forme avec un objectif clair : devenir Architecte Blockchain.</p>
             <div className='buttons'> 
                 <div className='button-container'>
-                    <a href="/chemin/vers/cv.pdf" download class="download-btn">CV</a>
+                    <a href="/chemin/vers/cv.pdf" download className="download-btn">CV</a>
                 </div>
                 <div className='button-container'>
-                    <a href="/chemin/vers/cv.pdf" download class="download-btn">GitHub</a>
+                    <a href="/chemin/vers/cv.pdf" download className="download-btn">GitHub</a>
                 </div>
             </div>   
+        </div>
+        <div className='animation'>
+            <div className="menu-right-animation">
+                <div className="circle-1"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <span className="menu-word">Mots</span>
+            </div>
         </div>
     </section>
     )
