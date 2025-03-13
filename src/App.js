b98ea1853f22fe6cdaa84cd9165eed34d0241529
Laure <laure_ld@outlook.fr>
2025-03-13 14:39:31 +0100

    Correction des routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import Header from './components/header';
-import Main from './components/main';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header";
+import Main from "./components/main";
+import Footer from "./components/footer";
+import Info from "./components/info";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-      <Main />
-    </div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
