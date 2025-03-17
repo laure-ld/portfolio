27f45376c1a8473fe169c4156efa8095530ea5c5
Laure <laure_ld@outlook.fr>
2025-03-17 23:28:05 +0100

    Ajout du logo en header

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,10 @@
-import  { Link } from 'react-router-dom';
+import  { useNavigate } from 'react-router-dom';
 import React, {useState} from 'react';
 import '../styles/header.scss';
 
+
 function Header () {
+    const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(false);
     function handleOnClick() {
         setIsOpen(!isOpen);
@@ -10,7 +12,7 @@ function Header () {
 
     return (
         <section className="container">
-            <p>Portfolio</p>
+            <img src="/images/logo.png" alt="logo" className="logo cursor-pointer" onClick={() => navigate("/main")}/>
             <button aria-expanded={isOpen} aria-controls="menu" onClick={handleOnClick} className="burger-btn">
                     <svg  className='line' stroke="black" fill="none" strokeLinecap="round" height="28" width="28" xmlns="http://www.w3.org/2000/svg">
                         <line x1="4" y1="12" x2="25" y2="12" strokeWidth="2"/>
@@ -18,11 +20,13 @@ function Header () {
                         <line x1="4" y1="18" x2="25" y2="18" strokeWidth="2"/>
                     </svg>
             </button>
-            <nav id="menu" className={`menu ${isOpen ? 'open' : ''}`} >
+            <nav id="menu" className={`menu ${isOpen ? 'open' : ''}`} onClick={handleOnClick}>
                 <ul>
-                    <li><Link to="/">Mes projets</Link></li>
-                    <li><Link to="/info">À propos</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+                    <li><a href="#projets">Projets</a></li>
+                    <li><a href="#competence">Compétences</a></li>
+                    <li><a href="#soft">Soft skills</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#rs">Réseaux</a></li>
                 </ul>
             </nav>
         </section>
