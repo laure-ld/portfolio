464b1f5d6a2c8c5438f246ea29956bbef2702df7
Laure <laure_ld@outlook.fr>
2025-03-17 11:37:16 +0100

    Ajout de l'effet data dans carrousel pir le revoie

--- src/components/main.js
+++ src/components/main.js
@@ -2,12 +2,13 @@ import React from "react";
 import About from "./about";
 import Carrousel from "./projects";
 import Skills from "./skills";
+import data from "../data-projects.json";
 
 function Main() {
     return( 
         <section className="contenair-main">
             <About />
-            <Carrousel />
+            <Carrousel data={data}/>
             <Skills />
         </section>
     )
