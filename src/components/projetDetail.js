c834c8cce7d18bdc50dfa4df0f02bf76abdb4c76
Laure <laure_ld@outlook.fr>
2025-03-17 23:27:35 +0100

    Ajustement de la page projet

--- src/components/projetDetail.js
+++ src/components/projetDetail.js
@@ -1,35 +1,48 @@
 import { useParams } from "react-router-dom";
 import data from "../data-projects.json";
-import '../styles/projetDetail.scss';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCards } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/effect-cards";
+import "../styles/projetDetail.scss";
 
 const ProjetDetail = () => {
     const { id } = useParams();
     const projet = data.find(p => p.id === parseInt(id));
+
     if (!projet) {
         return <p>Projet non trouvé</p>;
     }
 
     return (
-        <sectiion className="projet">
-            <div className="project-detail">
-            <div className="pictureProjet">
-                <img src={projet.image} alt={projet.name} />
-                <img src={projet.image} alt={projet.name} />
-                <img src={projet.image} alt={projet.name} />
-                <img src={projet.image} alt={projet.name} />
+        <section className="projet">
+            <div className="projet-detail">
+                <Swiper
+                    effect="cards"
+                    grabCursor={true}
+                    modules={[EffectCards]}
+                    className="mySecondSwiper"
+                    height={300}
+                >
+                    {projet.projet.map((image, index) => (
+                        <SwiperSlide key={index} className="swipperImg">
+                            <img src={image} alt={`Image ${index + 1} du projet`}/>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <div className="infoProjet">
+                    <h2>{projet.name}</h2>
+                    <p><strong>Objectif : </strong> {projet.objectif}</p>
+                    <p><strong>Technologie utilisé :</strong> {projet.technologie}</p>
+                </div>
             </div>
-        
-            <div className="infoProjet">
-                <h2>{projet.name}</h2>
-                <p><strong>Objectif :</strong> {projet.objectif}</p>
-                <p><strong>Description :</strong> {projet.description}</p>
-                <p><strong>Langage utilisé :</strong> {projet.language}</p>
-            </div>
-            </div>
+
             <div className="detailProjet">
-                <p>n</p>
+                <p><strong>Description du projet :</strong>{projet.description}</p>
+                <p>{projet.retour_experience}</p>
             </div>
-        </sectiion>
+        </section>
     );
 };
 
