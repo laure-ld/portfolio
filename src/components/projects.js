3e5e9e2a971dc9217c8a3926c6f348bc7e49b0dd
Laure <laure_ld@outlook.fr>
2025-03-17 11:37:50 +0100

    Ajout de l'element data

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,20 +2,18 @@ import { Swiper, SwiperSlide} from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
-import data from '../data-projects.json';
 import SlideContent from './slideContent';
 
-function Carrousel() {
-    
-return (
-    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-    {data.map((slide, index) => (
-    <SwiperSlide key={index} className='img'>
-        <SlideContent slide={slide} />
-    </SwiperSlide>
-    ))}
-    </Swiper>
-)
+function Carrousel({ data }) {
+    return (
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {data.map((slide, index) => (
+        <SwiperSlide key={index} className='img'>
+            <SlideContent slide={slide} />
+        </SwiperSlide>
+        ))}
+        </Swiper>
+    )
 }
 
 export default Carrousel;
