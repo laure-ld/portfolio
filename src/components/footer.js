e537927b037df852f0a2b71e98ee4e63058e9ba1
Laure <laure_ld@outlook.fr>
2025-03-17 23:25:16 +0100

    maj des url des lien rs

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,11 +2,10 @@ import '../styles/footer.scss';
 
 function Footer() {
     return (
-        <div className="footer">
+        <div className="footer" id='rs'>
             <p className="text-footer">À bientôt</p>
             <div className='link-footer'>
-                <a href="mailto:contact@exemple.com" className="">Me contacter</a>
-                <a href="https://www.linkedin.com/in/ton-profil" target="_blank" rel="noopener noreferrer" className="">
+                <a href="https://www.linkedin.com/in/laure-loda?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer" className="">
                     <img src="/images/link.png" alt="linkedin" className="" />
                 </a>
                 <a href="https://github.com/laure-ld" target="_blank" rel="noopener noreferrer" className="">
