d56c09f680d12f232d2eaf43cd1c96172e138ce4
Laure <laure_ld@outlook.fr>
2025-03-17 11:38:44 +0100

    Correspondance avec la data.json

--- src/components/slideContent.js
+++ src/components/slideContent.js
@@ -1,21 +1,27 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useSwiperSlide } from "swiper/react";
 
 const SlideContent = ({ slide }) => {
     const [isExpanded, setIsExpanded] = useState(true);
     const { isActive } = useSwiperSlide();
+    const navigate = useNavigate();
 
     return (
-         <div
+         <div   
             className={`slide-container ${isExpanded ? "full" : "split"}`}
             onMouseEnter={() => setIsExpanded(false)}
             onMouseLeave={() => setIsExpanded(true)}
         >
             <img src={slide.image} alt={slide.alt} className="slide-image"/>
-            <div className="slide-info">
-                <h3>{slide.categorie}</h3>
-                <p>{slide.description}</p>
-            </div>
+            <button  onClick={() => navigate(`/projets/${slide.id}`)} className="page-project">
+                <div className="slide-info">
+                    <h3>{slide.name}</h3>
+                    <p>{slide.objectif}</p>
+                    <p>{slide.description}</p>
+                    <p>{slide.language}</p>
+                </div>
+            </button>
         </div>
     );
 };
