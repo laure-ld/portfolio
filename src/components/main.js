1048e2376f15924a2991291fd7206c84cef6b96f
Laure <laure_ld@outlook.fr>
2025-03-19 13:37:23 +0100

    Travail sur le responsive

--- src/components/main.js
+++ src/components/main.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import React from "react";
 import About from "./about";
 import Carrousel from "./projects";
@@ -6,12 +7,39 @@ import data from "../data-projects.json";
 import ContactForm from "./contact";
 
 function Main() {
+    const [isVisible, setIsVisible] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 400) {
+              setIsVisible(true);
+            } else {
+              setIsVisible(false);
+            }
+          };
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        };
+      }, []);
+
+      const scrollToTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      };
+
     return( 
         <section className="contenair-main">
             <About />
             <Carrousel data={data}/>
             <Skills />
             <ContactForm />
+            {isVisible && (
+                <button id="backToTop" className="back-to-top" aria-label="Retour en haut" onClick={scrollToTop}>
+                &#8638;
+                </button>
+            )}
         </section>
     )
 }
--- src/components/slideContent.js
+++ src/components/slideContent.js
@@ -16,14 +16,13 @@ const SlideContent = ({ slide }) => {
             <img src={slide.image} alt={slide.alt} className="slide-image"/>
             <button  onClick={() => navigate(`/projets/${slide.id}`)} className="page-project">
                 <div className="slide-info">
-                    <h3>{slide.name}</h3>
-                    <p>{slide.objectif}</p>
-                    <p>{slide.date}</p>
-                    <p>Technologie utilisé dans ce projet : </p>
-                    <ul>
-                        {slide.technologie.map((tech, index) => (
-                            <li key={index}>{tech}</li>
-                        ))}
+                    <h3>{slide.name}</h3> - <p className="date">{slide.date}</p>
+                    <p className="objectif">Objectif du projet : {slide.objectif}</p>
+                    <p className="tech-title">Technologies utilisées :</p>
+                    <ul className="tech-list">
+                    {slide.technologie.map((tech, index) => (
+                        <li key={index}>{tech}</li>
+                    ))}
                     </ul>
                 </div>
             </button>
