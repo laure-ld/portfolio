25643bff2a185a68ef0717074e05df9b331ebb9b
Laure <laure_ld@outlook.fr>
2025-03-31 23:57:38 +0200

    Ajustement carrosel

--- src/components/about.js
+++ src/components/about.js
@@ -9,7 +9,7 @@ function About() {
             <p>Passionnée par le développement web, j’ai d’abord suivi un parcours en psychologie et ergonomie des IA avant de me tourner pleinement vers cette voie.Aujourd’hui, je me forme avec un objectif clair : devenir architecte blockchain.</p>
             <div className='buttons'> 
                 <div className='button-container'>
-                    <a href="/CV.pdf" download className="download-btn">CV</a>
+                    <a href="./CV.pdf" download className="download-btn">CV</a>
                 </div>
                 <div  onClick={() => window.open("https://github.com/laure-ld", "_blank")} className='button-container'>
                     <img src="/images/github.png" alt="GitHub"/>
